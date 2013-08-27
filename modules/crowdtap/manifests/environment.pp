fa3f8f3e353bbbcbf35706e8e178090421f0fcaf
Michael D'Auria <michael.dauria@gmail.com>
2013-08-27 12:44:24 -0400

    Revert "Install coffescript along with node v10"
    This reverts commit 6131c1b818df44b7873d420baec4cdfaf995d53a.

--- modules/crowdtap/manifests/environment.pp
+++ modules/crowdtap/manifests/environment.pp
@@ -10,10 +10,6 @@ class crowdtap::environment {
     node_version => 'v0.8.23'
   }
 
-  nodejs::module { 'coffee-script':
-    node_version => 'v0.10.13'
-  }
-
   include crowdtap::dotfiles
   include crowdtap::gitconfig
 
