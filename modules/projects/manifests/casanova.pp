053478e6e31fb6e4d026ab3b52791040144e1766
Robert Stern <roberts@crowdtap.com>
2014-03-05 10:32:53 -0500

    Update Casanova ruby version to 2.1.0-github1

--- modules/projects/manifests/casanova.pp
+++ modules/projects/manifests/casanova.pp
@@ -2,7 +2,7 @@ class projects::casanova {
   boxen::project { 'casanova':
     mongodb   => true,
     redis     => true,
-    ruby      => '2.0.0-p247',
+    ruby      => '2.1.0-github1',
     source    => 'crowdtap/casanova'
   }
 }
