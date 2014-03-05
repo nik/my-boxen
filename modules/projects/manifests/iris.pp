febfeaffc6820a777c27a63e54d8669b43075ca2
Kevin Cunningham <kevinc@crowdtap.com>
2014-03-05 15:43:29 -0500

    Update iris ruby version

--- modules/projects/manifests/iris.pp
+++ modules/projects/manifests/iris.pp
@@ -2,7 +2,7 @@ class projects::iris {
   boxen::project { 'iris':
     mongodb   => true,
     redis     => true,
-    ruby      => '1.9.3-p392',
+    ruby      => '2.1.0-github1',
     source    => 'crowdtap/iris'
   }
 }
